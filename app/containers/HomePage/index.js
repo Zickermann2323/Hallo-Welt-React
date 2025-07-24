c8ae04bde51ecf893e5e78259b5e1ce6c4ff1060
Paul21212 <paul.zickermann@gmx.de>
2025-07-24 09:53:21 +0200

    Update index.js

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -4,13 +4,10 @@ import { Helmet } from 'react-helmet';
 
 export default function HomePage() {
   return (
-    <article>
-      <Helmet>
-        <title>Ball Game</title>
-        <meta name="description" content="Simple ball guessing game" />
-      </Helmet>
-      <BallGame />
-    </article>
-  );
+  <div>
+    <BallGame />
+  </div>
+);
+
 }
 
